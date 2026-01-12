13c71dc3401871c23143381067824a44fc0d0c43
Luka Baksa <coaltail420@gmail.com>
2026-01-12 19:02:40 +0100

    fix: Change isAdmin flag to bool

---
+++ mshop-account-service/models/user.go
@@ -0,0 +1,25 @@
+package models
+
+// UpdateUserRequest represents basic user info that regular users can update
+type UpdateUserRequest struct {
+	FirstName   *string `json:"first_name"`
+	LastName    *string `json:"last_name"`
+	DateOfBirth *string `json:"date_of_birth"`
+	PhoneNumber *string `json:"phone_number"`
+	Address     *string `json:"address"`
+	Email       *string `json:"email"`
+}
+
+// AdminUpdateUserRequest represents all fields that admins can update
+type AdminUpdateUserRequest struct {
+	FirstName   *string `json:"first_name"`
+	LastName    *string `json:"last_name"`
+	DateOfBirth *string `json:"date_of_birth"`
+	PhoneNumber *string `json:"phone_number"`
+	Address     *string `json:"address"`
+	Email       *string `json:"email"`
+	Username    *string `json:"username"`
+	Role        *string `json:"role"`
+	IsAdmin     *bool   `json:"is_admin"`
+	IsActive    *bool   `json:"is_active"`
+}
